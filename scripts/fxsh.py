3f413af3f664d3583677714642bc80dc6b0cd17b
Ben Glick <glick@glick.cloud>
2020-12-04 16:39:07 -0600

    yapf

--- scripts/fxsh.py
+++ scripts/fxsh.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-
 """
 FuncX Shell - `fxsh`
 
 Use FuncX to open a virtual
-	interactive session on a FuncX endpoint.
+    interactive session on a FuncX endpoint.
 
 Any commands input will be forwarded to the
-	endpoint using `subprocess.check_output`
+    endpoint using `subprocess.check_output`
 
 Has only been tested with Linux-based endponits
 """
@@ -17,12 +16,13 @@ import argparse
 from blt_funcx_toolkit.execution import fxsh
 from blt_funcx_toolkit.config import blt_endpoints
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument("endpoint_name", "ep", type=str,
-					help="Endpoint to open interactive session on",
-					default="blt_small",
-					choices=blt_endpoints.keys())
+parser.add_argument("endpoint_name",
+                    "ep",
+                    type=str,
+                    help="Endpoint to open interactive session on",
+                    default="blt_small",
+                    choices=blt_endpoints.keys())
 args = parser.parse_args()
 
 fxsh(endpoint=args.endpoint_id)
